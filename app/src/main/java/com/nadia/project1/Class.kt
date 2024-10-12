b88468ad78b88a4a206ff69238d063e185207968
veresoora <lahanabira@gmail.com>
2024-10-12 15:53:21 +0700

    Commit 3: Membuat fungsi utama

--- app/src/main/java/com/nadia/project1/Class.kt
+++ app/src/main/java/com/nadia/project1/Class.kt
@@ -3,3 +3,17 @@ package com.nadia.project1
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "nadiamuna@gmail.com")
+
+    // Mencetak nilai properti: email
+    println(contact.email)
+
+    // Memperbarui nilai properti: email
+    contact.email = "haliza@gmail.com"
+
+    // Mencetak nilai baru dari properti: email
+    println(contact.email)
+
+}
