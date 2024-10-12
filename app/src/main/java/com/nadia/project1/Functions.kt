b511c96b9306d0aab442b201ed2300fc7b18dfba
veresoora <lahanabira@gmail.com>
2024-10-12 17:16:06 +0700

    Commit 14: Membuat fungsi utama dengan named argument dan default parameter value

--- app/src/main/java/com/nadia/project1/Functions.kt
+++ app/src/main/java/com/nadia/project1/Functions.kt
@@ -1,6 +1,6 @@
 package com.nadia.project1
 
-fun withoutParamater() {
+fun withoutParameter() {
     println("== withoutParameter ==")
     println("Hello, World!")
 }
@@ -9,4 +9,31 @@ fun withParameter(name: String) {
     println()
     println("== withParameter ==")
     println("Hello, $name!")
+}
+
+// named argument
+// named argument adalah cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello $name, your $age years old")
+}
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "nadia", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello $name, your $age years old")
+}
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("Haliza")
+    withNamedArgument(name = "Haliza", age = 20)
+    withDefaultParameter(age = 20)
 }
