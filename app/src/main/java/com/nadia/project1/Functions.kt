3e6a3740f44dc003d76b1de7fca72b88d6c13323
veresoora <lahanabira@gmail.com>
2024-10-12 17:02:08 +0700

    Commit 13: Membuat fungsi tanpa Parameter dan dengan Parameter

---
+++ app/src/main/java/com/nadia/project1/Functions.kt
@@ -0,0 +1,12 @@
+package com.nadia.project1
+
+fun withoutParamater() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
