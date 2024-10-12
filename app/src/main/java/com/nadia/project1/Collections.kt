cfcd51a456b3f9ecc8f220327111a75b69ac32d5
veresoora <lahanabira@gmail.com>
2024-10-12 16:16:07 +0700

    Commit 5: Set dengan setOf (Read-Only) dan mutableSetOf (Mutable)

--- app/src/main/java/com/nadia/project1/Collections.kt
+++ app/src/main/java/com/nadia/project1/Collections.kt
@@ -32,3 +32,34 @@ fun ContohList()  {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+// Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+// Set bersifat unordered, artinya data yang dimasukan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable menggunakan mutableSetOf
+
+fun ContohSet() {
+    println()
+    println("=== Set ==")
+
+    // Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke set mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    // Menghapus data dari set mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
